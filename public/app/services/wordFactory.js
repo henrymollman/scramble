6a86a88966a10c27fa2b17046228b4fe5900b671
henrymollman <hmollman@gmail.com>
2015-07-07 15:19:24 -0700

    Added anagram check

--- public/app/services/wordFactory.js
+++ public/app/services/wordFactory.js
@@ -1,6 +1,6 @@
 angular.module('scramble')
   .factory('WordFactory', function($http, $rootScope) {
-    var wordURL = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&excludePartOfSpeech=proper-noun&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=8&api_key=f8de9c37abe50fa5b1008000f7d0af42eca8c8c633958489a";
+    var wordURL = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&excludePartOfSpeech=proper-noun&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=5&api_key=f8de9c37abe50fa5b1008000f7d0af42eca8c8c633958489a";
 
 
       var wordnik = "WELCOME".split('');
@@ -40,17 +40,66 @@ angular.module('scramble')
         return wordnik;
       };
 
+      var getLetterCount = function(word) {
+        if (typeof word !== 'object')  {
+          word = word.split('');
+        }
+
+        return word.reduce(function(acc, letter) {
+          if (acc[letter]) {
+              acc[letter]++;
+              return acc;
+            }
+            else {
+              acc[letter] = 1;
+              return acc;
+            }
+        }, {});
+      };
+
       var checkWord = function() {
-        if (wordnik.join('') === originalWord.toUpperCase()) {
-          result = true;
-          success();
-        }
-        else {
-          result = false;
-          wordnik = originalWord.toUpperCase().split('');
-          wordBroadcast(wordnik);
-          fail();
-        }
+        // if (wordnik.join('') === originalWord.toUpperCase()) {
+          var word = wordnik.join('').toLowerCase();
+          var url = "http://api.wordnik.com:80/v4/word.json/" + word + "/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=f8de9c37abe50fa5b1008000f7d0af42eca8c8c633958489a";
+          var obj = getLetterCount(wordnik);
+          var originalWordArray = getLetterCount(originalWord.toUpperCase());
+          var objKeys = Object.keys(obj);
+          console.log('obj = ' + JSON.stringify(obj) + '\n originalWordArray = ' + JSON.stringify(originalWordArray) + '\n objKeys = ' + objKeys);
+          if (objKeys.reduce(function(acc, item) {
+            if (!acc) return false;
+            if (obj[item] === originalWordArray[item]) return true;
+            else return false;
+          })) {
+            console.log('calling http get for ' + wordnik.join(''));
+        $http.get(url)
+          .success(function(data) {
+              console.log(JSON.stringify(data) + " " + data.length);
+            if (data.length) {
+              console.log('there is a word');
+              result = true;
+              success();
+            }
+
+            else {
+              console.log('no word matched from api');
+              result = false;
+              wordnik = originalWord.toUpperCase().split('');
+              wordBroadcast(wordnik);
+              fail();
+            }
+          })
+
+          .error(function(data, status, headers, config) {
+            console.log('error! ' + data);
+          });
+          }
+          else {
+            console.log('not a match/anagram');
+                          result = false;
+              wordnik = originalWord.toUpperCase().split('');
+              wordBroadcast(wordnik);
+              fail();
+          }
       };
 
       var getResult = function() {
